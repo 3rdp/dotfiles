b4121da50a2a83289268038140a7d8adfb2e2240
3rdp <third.pancake16@gmail.com>
2017-07-14 01:44:16 +0300

    Changed apostrophe in vim

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@ sudo apt install vim-gtk-py2
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/joonty/vdebug ~/.vim/bundle/vdebug 
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes 
+# change apostrophe in vim's ukrainian-jcu file
+sudo sed -i '/`/c\`       ’' $(locate ukrainian-jcuken)
+
+git clone https://github.com/3rdp/dotfiles ~/dotfiles
+cd
+mv dotfiles/* dotfiles/.* .
