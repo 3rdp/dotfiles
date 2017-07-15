b923f7f0b43f95da392c8f946c0d435eb121a50c
3rdp <third.pancake16@gmail.com>
2017-07-15 17:34:27 +0300

    Took some goodies from fidblog.ru

--- install.sh
+++ install.sh
@@ -12,3 +12,14 @@ cd
 mv dotfiles/* dotfiles/.* .
 
 sudo tasksel install lamp-server
+
+# сюди ще треба ті штуки звідси http://fidblog.ru/%D0%BD%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B0-xubuntu-16-04-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5-%D1%83%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B8/
+sudo apt install -y vlc audacity gimp gimp-data-extras caffeine bleachbit gdebi
+sudo add-apt-repository ppa:linrunner/tlp
+sudo apt-get update && sudo apt install tlp tlp-rdw smartmontools ethtool && sudo tlp start
+sudo add-apt-repository ppa:libreoffice/ppa
+sudo add-apt-repository ppa:ravefinity-project/ppa
+sudo apt-get update && sudo apt-get install vibrancy-colors 
+for d in /usr/share/icons/*; do sudo gtk-update-icon-cache -f $d; done
+sudo fc-cache -fv
+mkdir ~/.compose-cache
