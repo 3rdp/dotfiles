f976224e0a853697a8b5be63441726aea29654b5
3rdp <third.pancake16@gmail.com>
2018-01-27 20:55:47 +0000

    Tried on Vagrant

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt install -y vim-gtk-py2 screen
 # install pathogen plugin manager
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -16,6 +17,7 @@ sudo tasksel install lamp-server
 # сюди ще треба ті штуки звідси http://fidblog.ru/%D0%BD%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B0-xubuntu-16-04-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5-%D1%83%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B8/
 sudo apt install -y vlc audacity gimp gimp-data-extras caffeine bleachbit gdebi
 # manage battery life better
+echo "Desktop setup now"
 sudo add-apt-repository ppa:linrunner/tlp
 sudo apt-get update && sudo apt install tlp tlp-rdw smartmontools ethtool && sudo tlp start
 # so office updates regularly
